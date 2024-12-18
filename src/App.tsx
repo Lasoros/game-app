97e0f50356039bc067c51c09ad836f6179ec507d
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-18 13:16:56 -0500

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-  return <Button>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "base"`,
+        lg: `"nav nav" "aside main"`, // wider than 1024px
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <GridItem hideBelow={"lg"} area={"aside"} padding={"10px"} bg={"gold"}>
+        Aside
+      </GridItem>
+      <GridItem area={"main nav"} padding={"10px"} bg={"blue"}>
+        GameGrid
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+export const NavBar = () => {
+  return (
+    <HStack justifyContent={"space-between"} padding={"10px"}>
+      <Image src={logo} boxSize={"60px"}></Image>
+      <Text>NavBar</Text>
+      {/* <ColorModeSwitch /> */}
+    </HStack>
+  );
+};
