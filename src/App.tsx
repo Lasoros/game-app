10ea1ca46310d6113e0b3bfa56f86c12dd077ec5
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-19 13:18:35 -0500

    Genre Fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -9,22 +10,15 @@ function App() {
         base: `"nav" "base"`,
         lg: `"nav nav" "aside main"`, // wider than 1024px
       }}
-      // templateColumns={{
-      //   base: "1fr",
-      //   lg: "250px 1fr",
-      // }}
     >
       <GridItem area={"nav"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"}>Aside</GridItem>
+        <GridItem area={"aside"}>
+          <GenreList />
+        </GridItem>
       </Show>
-      {/* <GridItem hideBelow={"lg"} area={"aside"}>
-        Aside
-      </GridItem> */}
-      {/* Above used prior to package.json*/}
-
       <GridItem area="main">
         <GameGrid />
       </GridItem>
