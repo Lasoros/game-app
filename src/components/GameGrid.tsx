b129a205ed9837f27db09f58ee080131ccef3608
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-18 20:15:31 -0500

    API Now Populates Games - Revert Code to Comment

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,52 @@
-import { Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+// import { Text } from "@chakra-ui/react";
+// import useGames from "../hooks/useGames";
+
+// const GameGrid = () => {
+//   const { games, error } = useGames();
+
+//   return (
+//     <>
+//       {error && <Text>{error}</Text>}
+//       <ul>
+//         {games.map((game) => (
+//           <li key={game.id}> {game.name} </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default GameGrid;
+
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const [games, setGames] = useState<Games[]>([]);
+  const [error, setError] = useState("");
+
+  interface Games {
+    id: number;
+    name: string;
+  }
+
+  interface FetchGamesResponse {
+    count: number;
+    results: Games[];
+  }
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  });
 
   return (
     <>
-      {error && <Text>{error}</Text>}
       <ul>
-        {games.map((game) => (
-          <li key={game.id}> {game.name} </li>
+        {games.map((games) => (
+          <li key={games.id}>{games.name}</li>
         ))}
       </ul>
     </>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,20 +1,17 @@
+// import axios from "axios";
+
+// export default axios.create ({
+//     baseURL: "https://api.rawg.io/api",
+//     params:{
+//         key: "9adb761b7a0a453898fc576545671931",
+//     },
+// });
+
 import axios from "axios";
 
-export default axios.create ({
-    baseURL: "https://api.rawg.io/api",
-    params:{
-        key: "9adb761b7a0a453898fc576545671931"
-    }
-})
-
-// export default axios.create
-// const apiClient = {
-//     get: (url: string) => {
-//         ({
-//             baseURL: "https://api.rawg.io/api",
-//             params:{
-//                 key: "9adb761b7a0a453898fc576545671931"
-//             }
-//         })
-//     }
-//   };
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "71a9b3d6c37b4d2c9c1d06bfebcbb9a7",
+  },
+});
