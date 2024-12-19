076b098e3b4fb8c158250b719d4b66f6e278a786
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-19 11:26:20 -0500

    Custom Hook Created For Game Fetch - Working API

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,52 +1,15 @@
-// import { Text } from "@chakra-ui/react";
-// import useGames from "../hooks/useGames";
-
-// const GameGrid = () => {
-//   const { games, error } = useGames();
-
-//   return (
-//     <>
-//       {error && <Text>{error}</Text>}
-//       <ul>
-//         {games.map((game) => (
-//           <li key={game.id}> {game.name} </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// };
-
-// export default GameGrid;
-
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Games[]>([]);
-  const [error, setError] = useState("");
-
-  interface Games {
-    id: number;
-    name: string;
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Games[];
-  }
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
+      {error && <Text>{error}</Text>}
       <ul>
-        {games.map((games) => (
-          <li key={games.id}>{games.name}</li>
+        {games.map((game) => (
+          <li key={game.id}> {game.name} </li>
         ))}
       </ul>
     </>
@@ -54,3 +17,41 @@ const GameGrid = () => {
 };
 
 export default GameGrid;
+
+// import React, { useEffect, useState } from "react";
+// import apiClient from "../services/api-client";
+
+// const GameGrid = () => {
+//   const [games, setGames] = useState<Games[]>([]);
+//   const [error, setError] = useState("");
+
+//   interface Games {
+//     id: number;
+//     name: string;
+//   }
+
+//   interface FetchGamesResponse {
+//     count: number;
+//     results: Games[];
+//   }
+
+//   useEffect(() => {
+//     apiClient
+//       .get<FetchGamesResponse>("/games")
+//       .then((res) => setGames(res.data.results))
+//       .catch((err) => setError(err.message));
+//   });
+
+//   return (
+//     <>
+//       <ul>
+//         {games.map((games) => (
+//           <li key={games.id}>{games.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default GameGrid;
+// Testing purposes to debug API not working - keep to quick test in case API stops working
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,10 @@
 // import axios from "axios";
-
 // export default axios.create ({
 //     baseURL: "https://api.rawg.io/api",
 //     params:{
 //         key: "9adb761b7a0a453898fc576545671931",
 //     },
-// });
+// }); Old API key wasnt working - renewed API works fine
 
 import axios from "axios";
 
