c07ed5e7a5633fc1d49504071fb5035cbbc5a688
Thomas Wimer <ipodtouch_guy@hotmail.com>
2024-12-23 13:20:09 -0500

    Handling Of GamesW/out Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,3 +1,5 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
+
 const getCroppedImageUrl = (url: string) => {
 
     // const target = 'media/'; //better to replace multiple uses with const value
@@ -7,7 +9,7 @@ const getCroppedImageUrl = (url: string) => {
 
     const target = 'media/';
 
-    if (!url) return " ";
+    if (!url) return noImage;
     
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
